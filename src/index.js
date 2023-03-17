02f614df222ec46ffb79729502a21a8cee1395a7
55vishal <55.vishalmalusare@gmail.com>
2023-03-17 18:20:43 +0530

    js p

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
+
 import Home from "./Pages/Home/Home";
 import NavBar from "./Components/Nav Bar/NavBar";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
